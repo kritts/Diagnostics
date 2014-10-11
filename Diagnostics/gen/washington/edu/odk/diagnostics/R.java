10f22638a24a69d2669c062278cb5c5384dffdf0
Krittika DSilva <krittika.dsilva@gmail.com>
2014-10-11 15:59:13 -0700

    Working on main structure of application

--- Diagnostics/gen/washington/edu/odk/diagnostics/R.java
+++ Diagnostics/gen/washington/edu/odk/diagnostics/R.java
@@ -1077,7 +1077,8 @@ containing a value of this type.
         public static final int abc_textfield_searchview_holo_light=0x7f020054;
         public static final int abc_textfield_searchview_right_holo_dark=0x7f020055;
         public static final int abc_textfield_searchview_right_holo_light=0x7f020056;
-        public static final int ic_launcher=0x7f020057;
+        public static final int button_config=0x7f020057;
+        public static final int ic_launcher=0x7f020058;
     }
     public static final class id {
         public static final int action_bar=0x7f05001c;
@@ -1091,10 +1092,11 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003c;
+        public static final int action_settings=0x7f05003e;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
+        public static final int camera=0x7f05003d;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int default_activity_button=0x7f050028;
@@ -1105,6 +1107,7 @@ containing a value of this type.
         public static final int end=0x7f050013;
         public static final int expand_activities_button=0x7f050026;
         public static final int expanded_menu=0x7f05002c;
+        public static final int gallery=0x7f05003c;
         public static final int home=0x7f050014;
         public static final int homeAsUp=0x7f050005;
         public static final int icon=0x7f05002a;
--- Diagnostics/src/washington/edu/odk/diagnostics/MainActivity.java
+++ Diagnostics/src/washington/edu/odk/diagnostics/MainActivity.java
@@ -4,14 +4,39 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
 
+	private Button mGallery;
+	
+	private Button mCamera;  
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+
+        mGallery = (Button) findViewById(R.id.gallery);
+        mCamera = (Button) findViewById(R.id.camera);
+        
+        mGallery.setOnClickListener(new OnClickListener() { 
+			@Override
+			public void onClick(View arg) { 
+				 
+			}  
+		}); 
+        
+        mCamera.setOnClickListener(new OnClickListener() { 
+			@Override
+			public void onClick(View arg) { 
+				 
+			}  
+		}); 
     }
 
 
