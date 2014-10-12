33e4d68213969eddc8ffaedd57df032fefb2bc47
Krittika DSilva <krittika.dsilva@gmail.com>
2014-10-12 14:47:47 -0700

    Added second activity that shows the image after it has been chosen

--- Diagnostics/gen/washington/edu/odk/diagnostics/R.java
+++ Diagnostics/gen/washington/edu/odk/diagnostics/R.java
@@ -1244,15 +1244,14 @@ containing a value of this type.
         /**  Description of a share target (both in the list of such or the default share button) in a ShareActionProvider (share UI). [CHAR LIMIT=NONE] 
          */
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
-        public static final int action_settings=0x7f0a0011;
+        public static final int action_settings=0x7f0a0012;
         /**  Home page 
          */
         public static final int app_name=0x7f0a000d;
         public static final int camera=0x7f0a0010;
         public static final int gallery=0x7f0a000e;
-        public static final int hello_world=0x7f0a0013;
         public static final int select=0x7f0a000f;
-        public static final int title_activity_process_image=0x7f0a0012;
+        public static final int title_activity_process_image=0x7f0a0011;
     }
     public static final class style {
         /** 
