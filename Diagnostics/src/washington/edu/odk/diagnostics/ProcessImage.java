687c37a6815d41f653b6d2ccffb0e08b63702646
Krittika DSilva <krittika.dsilva@gmail.com>
2014-10-29 22:17:04 -0700

    Cleaned up code

--- Diagnostics/src/washington/edu/odk/diagnostics/ProcessImage.java
+++ Diagnostics/src/washington/edu/odk/diagnostics/ProcessImage.java
@@ -2,18 +2,15 @@ package washington.edu.odk.diagnostics;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.LoaderCallbackInterface;
-import org.opencv.android.OpenCVLoader;
+import org.opencv.android.OpenCVLoader; 
 
 import android.support.v7.app.ActionBarActivity;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
+import android.graphics.BitmapFactory; 
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Button;
+import android.os.Environment;
+import android.util.Log; 
 import android.widget.ImageView;
 
 // This is the 
@@ -22,61 +19,54 @@ public class ProcessImage extends ActionBarActivity {
 
 	/** Used to show the users chosen image.*/
 	private Bitmap bitmap = null;
-	private ImageView im;
-	public native String findCirclesNative(String imagePath);
-	private boolean mOpenCVInitiated = false;
-	private String path;
 	
+	private ImageView im;
+	
+	private boolean mOpenCVInitiated = false;
+	
+	private String path;
+
+	private int resultCode;
+	
+	public native String findCirclesNative(String imagePath);
+	 
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_process_image);
-		OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_3, this, mLoaderCallback);
-		im = (ImageView) findViewById(R.id.image);
-        
 		
+		im = (ImageView) findViewById(R.id.image);
+		
+		OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_3, this, mLoaderCallback);
+         
 		Intent intent = getIntent();
 		Bundle extras = intent.getExtras();  
-		path  = extras.getString("path");  //TODO: Make sure absolute path;
+		path  = extras.getString("path");  //TODO: Make sure absolute path;  
+		resultCode = extras.getInt("resultCode");
+		
+		if(resultCode == 2) {
+			path = Environment.getExternalStorageDirectory() + "/Diagnostics_Images" + path;
+			Log.e(TAG, path);
+		}
 		Log.e(TAG, path);
-		Log.e(TAG, "!!!!!!!!!!!!!!!" + " " +  mOpenCVInitiated);
- 
 	}
 		
 	
-	private void setImage() { 
-		Log.e(TAG, path);
-		Log.e(TAG, "!!!!!!!!!!!!!!!" + " " +  mOpenCVInitiated);
-		
-		 
-		String output_path = findCirclesNative(path);
-		Log.e(TAG, output_path);
-		 
-		Log.e(TAG, output_path);
-		
-		bitmap = BitmapFactory.decodeFile(path); 
-		
-		 
+	private void setImage() {  
+		String output_path = findCirclesNative(path); 
+		bitmap = BitmapFactory.decodeFile(path);  
 
 		bitmap = BitmapFactory.decodeFile(output_path);
 		if(bitmap != null) {
 			while(bitmap.getHeight() > 2000 || bitmap.getWidth() > 2000) {  
+				Log.e(TAG, "Bitmap height: " + bitmap.getHeight() + " width: " + bitmap.getWidth());
 				bitmap = halfSize(bitmap);
-			} 
-			Log.e(TAG, "Bitmap height: " + bitmap.getHeight() + " width: " + bitmap.getWidth());
+			}  
 			im.setImageBitmap(bitmap);   
 		} 
 	}
-		
-	 
- 
-    
-    @Override
-    public void onResume() {
-        super.onResume();
-        
-    }
+		 
     
     private BaseLoaderCallback  mLoaderCallback = new BaseLoaderCallback(this) {
         @Override
